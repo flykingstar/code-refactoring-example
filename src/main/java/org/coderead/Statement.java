63fa9c171ceffcbd22058203baf31a41c6b38a2f
dengzhe <87505192@qq.com>
2021-04-17 11:09:38 +0800

    拆分方法表达意图

--- src/main/java/org/coderead/Statement.java
+++ src/main/java/org/coderead/Statement.java
@@ -25,45 +25,65 @@ public class Statement {
     }
 
     public String show() {
-        int totalAmount = 0;
-        int volumeCredits = 0;
         StringBuilder stringBuilder = new StringBuilder();
         String result = String.format("Statement for %s", invoice.getCustomer());
         stringBuilder.append(result);
         NumberFormat format = NumberFormat.getCurrencyInstance(new Locale("en", "US"));
 
+        int volumeCredits = 0;
+        int totalAmount = 0;
         for (Performance performance : invoice.getPerformances()) {
             Play play = plays.get(performance.getPlayId());
-            int thisAmount = 0;
+            int thisAmount;
             switch (play.getType()) {
                 case "tragedy":
-                    thisAmount = 40000;
-                    if (performance.getAudience() > 30) {
-                        thisAmount += 1000 * (performance.getAudience() - 30);
-                    }
+                    thisAmount = getTragedyAmount(performance);
                     break;
                 case "comedy":
-                    thisAmount = 30000;
-                    if (performance.getAudience() > 20) {
-                        thisAmount += 10000 + 500 *(performance.getAudience() - 20);
-                    }
-                    thisAmount += 300 * performance.getAudience();
+                    thisAmount = getComedyAmount(performance);
                     break;
                 default:
                     throw new RuntimeException("unknown type:" + play.getType());
             }
-
-            volumeCredits += Math.max(performance.getAudience() - 30, 0);
-
+            stringBuilder.append(String.format(" %s: %s (%d seats)\n", play.getName(), format.format(thisAmount / 100), performance.getAudience()));
+            totalAmount += thisAmount;
+            if ("tragedy".equals(play.getType())) {
+                volumeCredits += getTragedyVolumeCredits(performance);
+            }
             if ("comedy".equals(play.getType())) {
-                volumeCredits += Math.floor(performance.getAudience() / 5);
+                volumeCredits += getComedyVolumeCredits(performance);
             }
 
-            stringBuilder.append(String.format(" %s: %s (%d seats)\n", play.getName(), format.format(thisAmount/100), performance.getAudience()));
-            totalAmount += thisAmount;
         }
-        stringBuilder.append(String.format("Amount owed is %s\n", format.format(totalAmount/100)));
+        stringBuilder.append(String.format("Amount owed is %s\n", format.format(totalAmount / 100)));
         stringBuilder.append(String.format("You earned %s credits\n", volumeCredits));
         return stringBuilder.toString();
+    }
+
+    private double getComedyVolumeCredits(Performance performance) {
+        return Math.max(performance.getAudience() - 30, 0) + Math.floor(performance.getAudience() / 5);
+    }
+
+    private int getTragedyVolumeCredits(Performance performance) {
+        return Math.max(performance.getAudience() - 30, 0);
+    }
+
+    private int getComedyAmount(Performance performance) {
+        int thisAmount;
+        thisAmount = 30000;
+        if (performance.getAudience() > 20) {
+            thisAmount += 10000 + 500 * (performance.getAudience() - 20);
+        }
+        thisAmount += 300 * performance.getAudience();
+        return thisAmount;
+    }
+
+    private int getTragedyAmount(Performance performance) {
+        int thisAmount;
+        thisAmount = 40000;
+        if (performance.getAudience() > 30) {
+            thisAmount += 1000 * (performance.getAudience() - 30);
+        }
+        return thisAmount;
     }
 }
